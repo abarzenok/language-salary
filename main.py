241a9888381bf142f617c75040b8e60c4e541f4a
abarzenok <69277070+abarzenok@users.noreply.github.com>
2022-02-02 12:08:40 +0300

    Separate HH and SJ logic to different functions

--- main.py
+++ main.py
@@ -2,43 +2,30 @@ import os
 from dotenv import load_dotenv
 import requests
 from itertools import count
-from terminaltables import AsciiTable, DoubleTable, SingleTable
-from pprint import pprint  # debug only
+from terminaltables import AsciiTable
 
 
-def print_table(data):
-    title = "SJ salaries"
-
+def print_table(title, salaries_statistics):
     table_data = [
-        ["Язык программирования", "Вакансий найдено", "Вакансий обработано", "Средняя зарплата"],
+        [
+            "Язык программирования",
+            "Вакансий найдено",
+            "Вакансий обработано",
+            "Средняя зарплата"
+        ]
     ]
-
-    for language in data:
-        table_data.append([
-            language,
-            data[language]["vacancies_found"],
-            data[language]["vacancies_processed"],
-            data[language]["average_salary"]
-        ])
-
-    # AsciiTable.
+    for language in salaries_statistics:
+        table_data.append(
+            [
+                language,
+                salaries_statistics[language]["vacancies_found"],
+                salaries_statistics[language]["vacancies_processed"],
+                salaries_statistics[language]["average_salary"]
+            ]
+        )
     table_instance = AsciiTable(table_data, title)
-    table_instance.justify_columns[2] = 'right'
     print(table_instance.table)
     print()
-
-    # SingleTable.
-    table_instance = SingleTable(table_data, title)
-    table_instance.justify_columns[2] = 'right'
-    print(table_instance.table)
-    print()
-
-    # DoubleTable.
-    table_instance = DoubleTable(table_data, title)
-    table_instance.justify_columns[2] = 'right'
-    print(table_instance.table)
-    print()
-
 
 
 def get_all_hh_vacancies(language):
@@ -47,11 +34,9 @@ def get_all_hh_vacancies(language):
             language,
             page=page
         )
-
+        yield from page_response["items"]
         if page >= page_response["pages"]:
             break
-
-        yield from page_response["items"]
 
 
 def get_all_sj_vacancies(token, language):
@@ -61,38 +46,9 @@ def get_all_sj_vacancies(token, language):
             language,
             page=page
         )
-
         yield from page_response["objects"]
-
         if not page_response["more"]:
             break
-
-
-def get_sj_vacancies_page(token, language, page):
-    superjob_vacancies_api_url = "https://api.superjob.ru/2.0/vacancies/"
-    headers = {"X-Api-App-Id": token}
-    params = {
-        "town": "Moscow",
-        "catalogues": 33,  # Разработка, программирование "IT, Интернет, связь, телеком"
-        "count": 100,
-        "page": page,
-        "keywords[0][srws]": 10,
-        "keywords[0][skws]": "or",
-        "keywords[0][keys]": ["Программист", "Разработчик", "Developer"],
-        "keywords[1][srws]": 10,
-        "keywords[1][skws]": "particular",
-        "keywords[1][keys]": language
-    }
-    response = requests.get(
-        superjob_vacancies_api_url,
-        params=params,
-        headers=headers,
-    )
-    response.raise_for_status()
-
-    return response.json()
-
-
 
 
 def get_hh_vacancies_page(language, page=0):
@@ -106,9 +62,33 @@ def get_hh_vacancies_page(language, page=0):
             "page": page,
             "industry": 7,  # magic number
             "professional_role": 96,  # "name": "Программист, разработчик",
-            "area": 1, # Moscow
-            "period": 30, # Not older than days
+            "area": 1,  # Moscow
+            "period": 30,  # Not older than days
         }
+    )
+    response.raise_for_status()
+    return response.json()
+
+
+def get_sj_vacancies_page(token, language, page=0):
+    superjob_vacancies_api_url = "https://api.superjob.ru/2.0/vacancies/"
+    headers = {"X-Api-App-Id": token}
+    params = {
+        "town": "Moscow",
+        "catalogues": 33,
+        "count": 100,
+        "page": page,
+        "keywords[0][srws]": 10,
+        "keywords[0][skws]": "or",
+        "keywords[0][keys]": ["Программист", "Разработчик", "Developer"],
+        "keywords[1][srws]": 10,
+        "keywords[1][skws]": "particular",
+        "keywords[1][keys]": language,
+    }
+    response = requests.get(
+        superjob_vacancies_api_url,
+        params=params,
+        headers=headers,
     )
     response.raise_for_status()
     return response.json()
@@ -142,7 +122,7 @@ def predict_rub_salary_sj(vacancy):
 def predict_salary(salary_from, salary_to):
     predicted_salary = None
     if salary_from and salary_to:
-        predicted_salary = (salary_from+salary_to) / 2
+        predicted_salary = (salary_from + salary_to) / 2
     elif (not salary_from) and salary_to:
         predicted_salary = salary_to * 0.8
     elif salary_from and (not salary_to):
@@ -156,11 +136,57 @@ def calculate_avg_salary(vacancies, salary_predictor):
     salaries_sum = 0
     for vacancy in vacancies:
         predicted_salary = salary_predictor(vacancy)
-        if predicted_salary:
-            vacancies_processed += 1
-            salaries_sum += predicted_salary
-    avg_salary = int(salaries_sum/vacancies_processed) if vacancies_processed > 0 else 0
+        if not predicted_salary:
+            continue
+        vacancies_processed += 1
+        salaries_sum += predicted_salary
+    avg_salary = (
+        int(salaries_sum / vacancies_processed)
+        if vacancies_processed > 0 else 0
+    )
     return avg_salary, vacancies_processed
+
+
+def get_hh_statistics_table(languages):
+    languages_statistics = {}
+    for language in languages:
+        response = get_hh_vacancies_page(language)
+        if response["found"] < 100:  # magic number
+            continue
+        languages_statistics[language] = {}
+        vacancies = get_all_hh_vacancies(language)
+        languages_statistics[language]["vacancies_found"] = response["found"]
+        avg_salary, vacancies_processed = calculate_avg_salary(
+            vacancies,
+            predict_rub_salary_hh
+        )
+        languages_statistics[language]["average_salary"] = avg_salary
+        languages_statistics[language]["vacancies_processed"] = vacancies_processed
+    return languages_statistics
+
+
+def get_sj_statistics_table(token, languages):
+    languages_statistics = {}
+    for language in languages:
+        response = get_sj_vacancies_page(
+            token,
+            language,
+        )
+        if not response["total"]:  # magic number sj has too few vacancies
+            continue
+        languages_statistics[language] = {}
+        vacancies = get_all_sj_vacancies(
+            token,
+            language=language
+        )
+        languages_statistics[language]["vacancies_found"] = response["total"]
+        avg_salary, vacancies_processed = calculate_avg_salary(
+            vacancies,
+            predict_rub_salary_sj
+        )
+        languages_statistics[language]["average_salary"] = avg_salary
+        languages_statistics[language]["vacancies_processed"] = vacancies_processed
+    return languages_statistics
 
 
 def main():
@@ -181,38 +207,18 @@ def main():
         "Golang",
     ]
 
-    language_vacancies = {}
+    print_table(
+        "HeadHunter Moscow",
+        get_hh_statistics_table(top_programming_languages)
+    )
 
-
-    # for language in top_programming_languages:
-    #     response = get_hh_vacancies_page(language)
-    #     if response["found"] < 100: # magic number
-    #         continue
-    #     language_vacancies[language] = {} # avg_salary_stat
-    #     vacancies = get_all_hh_vacancies(language)
-    #     language_vacancies[language]["vacancies_found"] = response["found"]
-    #     avg_salary, vacancies_processed = calculate_avg_salary(vacancies, predict_rub_salary_hh)
-    #     language_vacancies[language]["average_salary"] = avg_salary
-    #     language_vacancies[language]["vacancies_processed"] = vacancies_processed
-
-    ##sj processing
-    for language in top_programming_languages:
-        response = get_sj_vacancies_page(api_key_superjob, language=language, page=0)
-        # if response["total"] < 100: # magic number sj has too few vacancies
-        #     continue
-        language_vacancies[language] = {} # avg_salary_stat
-        vacancies = get_all_sj_vacancies(api_key_superjob, language=language)
-        language_vacancies[language]["vacancies_found"] = response["total"]
-        avg_salary, vacancies_processed = calculate_avg_salary(vacancies, predict_rub_salary_sj)
-        language_vacancies[language]["average_salary"] = avg_salary
-        language_vacancies[language]["vacancies_processed"] = vacancies_processed
-
-    # pprint(language_vacancies)
-
-    print_table(language_vacancies)
-
-    # get_sj_vacancies_page(api_key_superjob, "Python", 0)
-
+    print_table(
+        "SuperJob Moscow",
+        get_sj_statistics_table(
+            api_key_superjob,
+            top_programming_languages
+        )
+    )
 
 
 if __name__ == "__main__":
